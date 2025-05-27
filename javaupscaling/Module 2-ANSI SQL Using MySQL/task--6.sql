45302d82c2e7d28ad4bebe4cb9d105362a18755e
Sriram Manish Chandra <manishsriram1234@gmail.com>
2025-05-27 21:23:53 +0530

    Delete javaupscaling directory

--- javaupscaling/Module 2-ANSI SQL Using MySQL/task--6.sql
+++
@@ -1,7 +0,0 @@
-SELECT e.title,
-       SUM(resource_type = 'pdf') AS pdfs,
-       SUM(resource_type = 'image') AS images,
-       SUM(resource_type = 'link') AS links
-FROM Events e
-LEFT JOIN Resources r ON e.event_id = r.event_id
-GROUP BY e.event_id;
